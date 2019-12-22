14a92776a22fee4c27a3d96425034afdc4601f85
Chris Waalberg <chris@q42.nl>
2019-12-22 17:57:08 +0100

    Initial Commit

---
+++ Paco Sako/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paco Sako
+//
+//  Created by Chris Waalberg on 22/12/2019.
+//  Copyright © 2019 Chris Waalberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Paco SakoTests/Paco_SakoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Paco_SakoTests.swift
+//  Paco SakoTests
+//
+//  Created by Chris Waalberg on 22/12/2019.
+//  Copyright © 2019 Chris Waalberg. All rights reserved.
+//
+
+import XCTest
+@testable import Paco_Sako
+
+class Paco_SakoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
